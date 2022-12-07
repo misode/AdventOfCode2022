2c613e0412c1b7c188e2748c9d2387e1be565a63
Misode <Misoloo64@gmail.com>
2022-12-07 07:01:44 +0100

    Simplify further

--- day7/script.py
+++ day7/script.py
@@ -2,20 +2,18 @@ from collections import defaultdict
 
 with open('day7/in.txt') as f:
 	lines = [l.strip() for l in f.readlines()]
-
 	path = []
 	used_size = 0
 	sizes = defaultdict(int)
 
 	for line in lines:
 		if line.startswith('$'):
-			if line.startswith('$ cd'):
-				if line.startswith('$ cd /'):
-					path = []
-				elif line.startswith('$ cd ..'):
-					path.pop()
-				else:
-					path.append(line.removeprefix('$ cd '))
+			if line.startswith('$ cd /'):
+				path = []
+			elif line.startswith('$ cd ..'):
+				path.pop()
+			elif line.startswith('$ cd'):
+				path.append(line.removeprefix('$ cd '))
 		elif not line.startswith('dir'):
 			size = int(line.split(' ')[0])
 			used_size += size
@@ -23,16 +21,9 @@ with open('day7/in.txt') as f:
 				key = ".".join(path[0:i+1])
 				sizes[key] += size
 
-	p1 = 0
-	for path in sizes:
-		if sizes[path] <= 100000:
-			p1 += sizes[path]
-	print(p1)
+	print(sum(s for s in sizes.values() if s <= 100000))
 
-	unused_size = 70000000 - used_size
-
-	A = sorted([sizes[path] for path in sizes])
-	for a in A:
-		if a >= 30000000 - unused_size:
-			print(a)
+	for s in sorted(sizes.values()):
+		if 70000000 - used_size + s >= 30000000:
+			print(s)
 			break
