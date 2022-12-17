d82e2f548f3605a1d2b426ee29ec20839d24d69c
Misode <Misoloo64@gmail.com>
2022-12-17 02:44:07 +0100

    Optimize part1 (? -> 1.5s, example ? -> 2ms)

--- day16/script.py
+++ day16/script.py
@@ -1,45 +1,51 @@
-from collections import defaultdict
 import re
-from dataclasses import dataclass
-import math
 import time
-from functools import lru_cache
-
-@dataclass(frozen=True)
-class Valve:
-  id: str
-  adj: list[str]
-  rate: int
 
 
-with open('day16/example.txt') as f:
+with open('day16/in.txt') as f:
   lines = [l.strip() for l in f.readlines()]
 
-  V: dict[str, Valve] = dict()
+  START = "AA"
+  MAXT = 30
+  V: dict[str, (int, list[str])] = dict()
 
   for line in lines:
     a, b, c = re.match("Valve (.+) has flow rate=(.+); tunnels? leads? to valves? (.*)", line).groups()
-    V[a] = Valve(a, c.split(', '), int(b))
+    V[a] = (int(b), c.split(', '))
 
-  @lru_cache(maxsize=None)
+  G: dict[str, list[(str, int)]] = dict()
+
+  for a in V:
+    if a != START and V[a][0] == 0:
+      continue
+    ADJ = dict()
+    Q = [(a, 0)]
+    S = set(a)
+    while Q:
+      u, d = Q.pop(0)
+      for v in V[u][1]:
+        if v in S:
+          continue
+        S.add(v)
+        if v == START or V[v][0] > 0:
+          ADJ[v] = d+1
+        Q.append((v, d+1))
+    G[a] = [(k, ADJ[k]) for k in ADJ]
+
   def search(a: str, t: int, open: tuple[str]):
-    if t > 30:
+    if t > MAXT or len(open) == len(G):
       return 0
-    releasing = sum(V[id].rate for id in open)
-    if len(open) == len(V):
-      return releasing
-
     best = 0
-
-    if a not in open:
-      best = max([best, search(a, t+1, (*open, a))]) # open A
-
-    for a2 in V[a].adj:
-      best = max([best, search(a2, t+1, open)]) # move to A2
-
-    return best + releasing
+    for a2, d2 in G[a]:
+      if a2 in open:
+        continue
+      b2 = search(a2, t+d2+1, (*open, a2))
+      score = b2 + V[a2][0] * (MAXT - (t+d2))
+      if score > best:
+        best = score
+    return best
 
   t0 = time.perf_counter()
-  print(search("AA", 1, ()))
+  print(search(START, 1, ("AA",)))
   t1 = time.perf_counter()
-  print(f"Took {round(t1-t0)} seconds")
+  print(f"Took {round((t1-t0)*1000)} ms")
