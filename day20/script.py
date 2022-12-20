cea3bd8cd0223c71e92075040862ac25322dcf6f
Misode <Misoloo64@gmail.com>
2022-12-20 07:10:23 +0100

    Day 20 part 2

--- day20/script.py
+++ day20/script.py
@@ -1,22 +1,15 @@
-from collections import deque
-import re
-
 with open('day20/in.txt') as f:
   lines = [l.strip() for l in f.readlines()]
 
-  nums = [(i, int(l)) for i, l in enumerate(lines)]
-  N = len(nums)
-  mixed = set()
-  i = 0
-  while len(mixed) < N:
-    n = nums[i]
-    if n in mixed:
-      i = (i + 1) % N
-      continue
-    nums.remove(n)
-    j = (i + n[1]) % (N-1)
-    nums.insert(j, n)
-    mixed.add(n)
-
-  zero = next((i for i, n in enumerate(nums) if n[1] == 0))
-  print(nums[(zero + 1000) % N][1] + nums[(zero + 2000) % N][1] + nums[(zero + 3000) % N][1])
+  N = len(lines)
+  for (rounds, multiplier) in [(1, 1), (10, 811589153)]:
+    nums = [(i, multiplier*int(l)) for i, l in enumerate(lines)]
+    for r in range(rounds):
+      for c in range(N):
+        i = next(j for j, n in enumerate(nums) if n[0] == c)
+        n = nums[i]
+        nums.remove(n)
+        j = (i + n[1]) % (N-1)
+        nums.insert(j, n)
+    zero = next(i for i, n in enumerate(nums) if n[1] == 0)
+    print(sum([nums[(zero + i) % N][1] for i in (1000, 2000, 3000)]))
